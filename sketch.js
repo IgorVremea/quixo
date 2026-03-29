730d5543e1020ddda7061d1cc97b9d933c344ad7
Bacter Sidonia <sidoniabacter17@gmail.com>
2026-03-29 15:39:10 +0300

    by mistake

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 let controller;
 
-function setup(){
-    createCanvas(CONFIG.canvas.width, CONFIG.canvas.height);
-    controller = new Controller(new Board());
+function setup() {
+  createCanvas(CONFIG.canvas.width, CONFIG.canvas.height);
+  controller = new Controller(new Board());
 }
 
 function draw() {
-    controller.tick();
+  controller.tick();
 }
 
-function mousePressed(){
-    let clickedCell = controller.board.getHoveredCell();
-    controller.cellClick(clickedCell);
+function mousePressed() {
+  let clickedCell = controller.board.getHoveredCell();
+  controller.cellClick(clickedCell);
 }
