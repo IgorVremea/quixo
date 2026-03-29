701ebfa7d93d7288e35271103f91136b20672f08
Bacter Sidonia <sidoniabacter17@gmail.com>
2026-03-29 15:27:09 +0300

    adding botton, need to be fixed

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
 let controller;
 
-function setup(){
-    createCanvas(CONFIG.canvas.width, CONFIG.canvas.height);
-    controller = new Controller(new Board());
+function setup() {
+  createCanvas(CONFIG.canvas.width, CONFIG.canvas.height);
+  controller = new Controller(new Board());
+  // apelare functia din botton.js care face butonul de restart si scorul sa apara pe ecran
+  createUI();
 }
 
 function draw() {
-    controller.tick();
+  controller.tick();
 }
 
-function mousePressed(){
-    let clickedCell = controller.board.getHoveredCell();
-    controller.cellClick(clickedCell);
+function mousePressed() {
+  let clickedCell = controller.board.getHoveredCell();
+  controller.cellClick(clickedCell);
 }
